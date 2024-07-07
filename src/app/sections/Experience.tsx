71685d0f4d68a385ecc01450cd7baf05ee6fa9ae
Rimsha Gul <rimshagulc@gmail.com>
2024-07-07 15:47:12 +0500

    feat: Make experience section responsive

--- src/app/sections/Experience.tsx
+++ src/app/sections/Experience.tsx
@@ -16,10 +16,10 @@ const Experience = () => {
         </h2>
         <div className="relative w-full">
           {/* Vertical Line */}
-          <div className="absolute inset-0 flex justify-center">
+          <div className="absolute inset-0 flex justify-start lg:justify-center pl-8 lg:pl-0">
             <div className="w-1 bg-turquoise h-full"></div>
           </div>
-          <div className="relative z-10 flex flex-col items-center">
+          <div className="relative z-10 flex flex-col items-start lg:items-center">
             {/* Central Icon */}
             <div className="flex justify-center items-center text-3xl text-secondary bg-secondary border-2 border-secondary rounded-full p-4 mb-28">
               <FaBuilding />
@@ -27,27 +27,37 @@ const Experience = () => {
             {experiences.map((exp, index) => (
               <div
                 key={index}
-                className="w-full flex justify-between items-center mb-36"
+                className={`w-full flex flex-col lg:flex-row justify-start lg:justify-between items-start mb-36`}
               >
                 {index % 2 === 0 ? (
                   <>
+                    {/** Timeline Image */}
+                    <div className="flex lg:hidden flex flex-col items-center pl-4">
+                      <div className="w-9 h-9 bg-secondary border-2 border-secondary rounded-full flex justify-center items-center">
+                        <span className="w-2.5 h-2.5 bg-turquoise rounded-full"></span>
+                      </div>
+                      <div className="w-1 bg-turquoise flex-1"></div>
+                    </div>
                     <motion.div
                       initial={{ opacity: 0, x: -20 }}
                       whileInView={{ opacity: 1, x: 0 }}
                       transition={{ duration: 1 }}
-                      className="w-1/2 px-4 text-right"
+                      className="w-full lg:w-1/2 text-start lg:text-right pl-20 lg:pl-0 -mt-8 lg:mt-0"
                     >
                       <h3 className="text-2xl xl:text-3xl font-bold text-secondary uppercase pb-2">
                         {exp.title}
                       </h3>
-                      <h4 className="text-xl xl:text-2xl font-light pb-2">
+                      <h4 className="text-xl xl:text-2xl uppercase font-semibold pb-2">
                         {exp.company}
                       </h4>
-                      <p className="text-sm xl:text-xl pb-4">{exp.duration}</p>
+                      <p className="xl:text-xl uppercase pb-4">
+                        {exp.duration}
+                      </p>
                     </motion.div>
-                    <div className="flex flex-col items-center px-4">
+                    {/** Timeline Image */}
+                    <div className="hidden lg:flex flex-col items-center px-6 pt-10">
                       <div className="w-9 h-9 bg-secondary border-2 border-secondary rounded-full flex justify-center items-center">
-                        <span className="w-3 h-3 bg-turquoise rounded-full"></span>
+                        <span className="w-2.5 h-2.5 bg-turquoise rounded-full"></span>
                       </div>
                       <div className="w-1 bg-turquoise flex-1"></div>
                     </div>
@@ -55,7 +65,7 @@ const Experience = () => {
                       initial={{ opacity: 0, x: 20 }}
                       whileInView={{ opacity: 1, x: 0 }}
                       transition={{ duration: 1 }}
-                      className="w-1/2 px-4"
+                      className="w-full lg:w-1/2 text-start lg:text-left pl-20 lg:pl-0"
                     >
                       {exp.description.map((desc, idx) => (
                         <p key={idx} className="text-sm xl:text-lg pb-2">
@@ -66,21 +76,10 @@ const Experience = () => {
                   </>
                 ) : (
                   <>
-                    <motion.div
-                      initial={{ opacity: 0, x: -20 }}
-                      whileInView={{ opacity: 1, x: 0 }}
-                      transition={{ duration: 1 }}
-                      className="w-1/2 px-4 text-right"
-                    >
-                      {exp.description.map((desc, idx) => (
-                        <p key={idx} className="text-sm xl:text-lg pb-2">
-                          {desc}
-                        </p>
-                      ))}
-                    </motion.div>
-                    <div className="flex flex-col items-center px-4">
+                    {/** Timeline Image */}
+                    <div className="flex lg:hidden flex flex-col items-center pl-4">
                       <div className="w-9 h-9 bg-secondary border-2 border-secondary rounded-full flex justify-center items-center">
-                        <span className="w-3 h-3 bg-turquoise rounded-full"></span>
+                        <span className="w-2.5 h-2.5 bg-turquoise rounded-full"></span>
                       </div>
                       <div className="w-1 bg-turquoise flex-1"></div>
                     </div>
@@ -88,22 +87,59 @@ const Experience = () => {
                       initial={{ opacity: 0, x: 20 }}
                       whileInView={{ opacity: 1, x: 0 }}
                       transition={{ duration: 1 }}
-                      className="w-1/2 px-4 text-left"
+                      className="w-full lg:hidden text-start pl-20 -mt-8"
                     >
                       <h3 className="text-2xl xl:text-3xl font-bold text-secondary uppercase pb-2">
                         {exp.title}
                       </h3>
-                      <h4 className="text-xl xl:text-2xl font-light pb-2">
+                      <h4 className="text-xl xl:text-2xl uppercase font-semibold pb-2">
                         {exp.company}
                       </h4>
-                      <p className="text-sm xl:text-xl pb-4">{exp.duration}</p>
+                      <p className="xl:text-xl uppercase pb-4">
+                        {exp.duration}
+                      </p>
+                    </motion.div>
+                    <motion.div
+                      initial={{ opacity: 0, x: -20 }}
+                      whileInView={{ opacity: 1, x: 0 }}
+                      transition={{ duration: 1 }}
+                      className="w-full lg:w-1/2 text-start lg:text-right pl-20 lg:pl-0"
+                    >
+                      {exp.description.map((desc, idx) => (
+                        <p key={idx} className="text-sm xl:text-lg pb-2">
+                          {desc}
+                        </p>
+                      ))}
+                    </motion.div>
+                    {/** Timeline Image */}
+                    <div className="hidden lg:flex flex-col items-center px-6 pt-10">
+                      <div className="w-9 h-9 bg-secondary border-2 border-secondary rounded-full flex justify-center items-center">
+                        <span className="w-2.5 h-2.5 bg-turquoise rounded-full"></span>
+                      </div>
+                      <div className="w-1 bg-turquoise flex-1"></div>
+                    </div>
+                    <motion.div
+                      initial={{ opacity: 0, x: 20 }}
+                      whileInView={{ opacity: 1, x: 0 }}
+                      transition={{ duration: 1 }}
+                      className="hidden lg:flex flex-col w-1/2 lg:text-left pl-20 lg:pl-0 -mt-8 lg:mt-0"
+                    >
+                      <h3 className="text-2xl xl:text-3xl font-bold text-secondary uppercase pb-2">
+                        {exp.title}
+                      </h3>
+                      <h4 className="text-xl xl:text-2xl uppercase font-semibold pb-2">
+                        {exp.company}
+                      </h4>
+                      <p className="xl:text-xl uppercase pb-4">
+                        {exp.duration}
+                      </p>
                     </motion.div>
                   </>
                 )}
               </div>
             ))}
             {/* Bottom Dot */}
-            <div className="w-5 h-5 bg-turquoise rounded-full -mt-16"></div>
+            <div className="w-5 h-5 bg-turquoise rounded-full -mt-16 ml-6 lg:ml-0"></div>
           </div>
         </div>
       </div>
